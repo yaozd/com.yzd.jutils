1a2f27ea82c73f338870916bb5cc8e95eee6085a
yzd <tedijoteamo@163.com>
2017-05-02 16:34:47 +0800

    update ObjectLock

--- com.yzd.jutils/src/main/java/com/yzd/jutils/objectLock/ObjectLock.java
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/objectLock/ObjectLock.java
@@ -16,10 +16,18 @@ public class ObjectLock {
     public LoadingCache<String, Object>  monitorCache;
     private ObjectLock (){
         PrintUtil.outLn("ObjectLock monitor cache init");
+        //todo 主要是测试方便查看-生产环境是可以把移除监听删除的
+        RemovalListener<String, Object> removalListener = new RemovalListener<String, Object>() {
+            @Override
+            public void onRemoval(RemovalNotification<String, Object> removalNotification) {
+                PrintUtil.outLn(removalNotification.getKey());
+            }
+        };
         //todo 只是为测试把maximumSize设置为1
         LoadingCache<String, Object> monitorCache = CacheBuilder.newBuilder()
                 .weakValues()
                 .maximumSize(1)
+                .removalListener(removalListener)
                 .build(new CacheLoader<String, Object>() {
                     @Override
                     public Object load(String s) {
