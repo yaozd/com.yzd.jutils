446113bf9427ee5507005457118150b75e587df7
yzd <tedijoteamo@163.com>
2017-07-14 16:03:22 +0800

    map-相同的KEY数据是替换的

--- com.yzd.jutils/src/main/java/com/yzd/jutils/collectionsExt/CollectionsTest.java
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/collectionsExt/CollectionsTest.java
@@ -1,12 +1,19 @@
 package com.yzd.jutils.collectionsExt;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by zd.yao on 2017/7/13.
  */
 public class CollectionsTest {
     public static void main(String[] args){
+        //相同的KEY数据是替换的
+        Map<String, Object> finalDataMap = new ConcurrentHashMap<>();
+        finalDataMap.put("now","1");
+        finalDataMap.put("now","3");
+        //finalDataMap.size()=1
+        System.out.println("finalDataMap.size()="+finalDataMap.size());
         //Collections unmodifiableMap 这个的作用是什么
         //不可修改集合
         //https://zhidao.baidu.com/question/448914033.html
