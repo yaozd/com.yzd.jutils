f48cc490209d15d105960a95f7dbd0a60f0bdabc
yzd <tedijoteamo@163.com>
2017-09-12 16:45:45 +0800

    分库分表的计算结果-整理版

--- com.yzd.jutils/src/main/java/com/yzd/jutils/guava/listExt/ListTest.java
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/guava/listExt/ListTest.java
@@ -8,6 +8,27 @@ import com.google.common.collect.Multimaps;
 import java.util.*;
 
 /**
+ * 参考：
+ *  Guava学习笔记：Guava新增集合类型-Multimap
+ *  http://www.cnblogs.com/peida/p/3180129.html
+ * Multimap的实现
+
+ 　　Multimap提供了丰富的实现，所以你可以用它来替代程序里的Map<K, Collection<V>>，具体的实现如下：
+ 　　Implementation            Keys 的行为类似       　　　Values的行为类似
+ 　　ArrayListMultimap         HashMap                   　　ArrayList
+ 　　HashMultimap               HashMap                  　　 HashSet
+ 　　LinkedListMultimap        LinkedHashMap*              LinkedList*
+ 　　LinkedHashMultimap      LinkedHashMap                LinkedHashSet
+ 　　TreeMultimap                TreeMap                          TreeSet
+ 　　ImmutableListMultimap  ImmutableMap                 ImmutableList
+ 　　ImmutableSetMultimap  ImmutableMap                 ImmutableSet
+
+
+ 以上这些实现，除了immutable的实现都支持null的键和值。
+ 　　1.LinkedListMultimap.entries()能维持迭代时的顺序。
+
+ 　　2.LinkedHashMultimap维持插入的顺序，以及键的插入顺序。
+ 　　要注意并不是所有的实现都正真实现了Map<K, Collection<V>>！（尤其是有些Multimap的实现为了最小话开销，使用了自定义的hash table）
  * Created by zd.yao on 2017/6/16.
  */
 public class ListTest {
--- com.yzd.jutils/src/main/java/com/yzd/jutils/shardingExt/_MainTest.java
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/shardingExt/_MainTest.java
@@ -1,79 +1,160 @@
 package com.yzd.jutils.shardingExt;
 
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import com.yzd.jutils.print.PrintUtil;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 目前没有写完没有参考意义
  * 后面可参考-【分库分表-总结-byArvin】在
  * 参考：一种可以避免数据迁移的分库分表scale-out扩容方式
  * http://www.cnblogs.com/tommyli/p/3767362.html
+ * 目前是一个库2个表的结构
  * Created by zd.yao on 2017/8/24.
  */
 public class _MainTest {
+
     public static void main(String[] args) {
+        //目前是一个库2个表的结构
+        //目前的分段是20、40、80、160
+        dbRuleTest();
+        tbRuleTest();
+        ruleTest_DB_TB();
+        ruleTest_DB_TB2();
+        int pause = 0;
+    }
+    //分库分表的计算结果-整理版
+    private static void ruleTest_DB_TB2(){
+        PrintUtil.outLn("分库分表的计算结果-整理版");
+        Integer id = 0;
+        //分库分表的计算结果
+        //LinkedListMultimap与LinkedHashMultimap有先后序顺的集合
+        Multimap<String, String> shardMap = LinkedListMultimap.create();
+        //Multimap<String, String> shardMap = LinkedHashMultimap.create();
+        //
+        for (; id < 160; id++) {
+            String dbVal = getDBInfoByUserId(id);
+            String tbVal = getTableInfoByUserId(id);
+            boolean isExist= shardMap.get(dbVal).contains(tbVal);
+            if(isExist){
+                continue;
+            };
+            shardMap.put(dbVal, tbVal);
+            PrintUtil.outLn("dbVal="+dbVal+"|"+"tbVal="+tbVal);
+        }
+        PrintUtil.outLn("输出分库分表的计算结果:");
+        for(Map.Entry<String, Collection<String>> e : shardMap.asMap().entrySet())
+        {
+            String dbVal= e.getKey();
+            System.out.print(dbVal+"= ");
+            List<String> itemList= Lists.newArrayList(e.getValue());
+            int n=0;
+            for(String i:itemList){
+                System.out.print(i);
+                if(n==0){
+                    System.out.print(" | ");
+                    n++;
+                }
+            }
+            PrintUtil.outLn("");
+        }
+        int pause = 0;
+    }
+    //分库分表的计算结果
+    private static void ruleTest_DB_TB(){
+        Integer id = 0;
+        //分库分表的计算结果
+        Multimap<String, String> shardMap = ArrayListMultimap.create();
+        //
+        for (; id < 160; id++) {
+            String dbVal = getDBInfoByUserId(id);
+            String tbVal = getTableInfoByUserId(id);
+            shardMap.put(dbVal, tbVal);
+            PrintUtil.outLn("dbVal="+dbVal+"|"+"tbVal="+tbVal);
+        }
+        int pause = 0;
+    }
+    private static void tbRuleTest() {
+        Integer id = 0;
+        Multimap<String, Integer> tbMap = ArrayListMultimap.create();
+        //
+        for (; id < 160; id++) {
+            String val = getTableInfoByUserId(id);
+            tbMap.put(val, id);
+            PrintUtil.outLn(getTableInfoByUserId(id));
+        }
+        int pause = 0;
+    }
+    //region 版本1-表分片计算规则
+    //以用户id作为唯度
+    private static String getTableInfoByUserId(int id) {
+        if (id < 10) {
+            //2个表
+            return "t" + (id % 2);
+        }
+        if (id < 20) {
+            //4个表
+            return "t" + (id % 2) + "_1";
+        }
+        if (id < 40) {
+            //8个表
+            return "t" + (id % 4) + "_2";
+        }
+        if (id < 80) {
+            //16个表
+            return "t" + (id % 8) + "_3";
+        }
+        if (id < 160) {
+            //32个表
+            return "t" + (id % 16) + "_4";
+        }
+        throw new IllegalArgumentException("[tbRule]id out of range. id:" + id);
+    }
+    //endregion
+    private static void dbRuleTest() {
         Integer id = 0;
         Multimap<String, Integer> dbMap = ArrayListMultimap.create();
         ;
         //
         for (; id < 160; id++) {
-            String val = getVal(id);
+            String val = getDBInfoByUserId(id);
             dbMap.put(val, id);
-            PrintUtil.outLn(getVal(id));
+            PrintUtil.outLn(getDBInfoByUserId(id));
         }
         int pause = 0;
     }
 
-    private static String getVal(int id) {
-        if (id < 10) {
-            //2个库
-            return "db" + (id % 2);
-        }
-        if (id < 20) {
-            //4个库
-            return "db" + ((id % 2) + 2);
-        }
-        if (id < 40) {
-            //8个库
-            return "db" + ((id % 4) + 4);
-        }
-        if (id < 80) {
-            //16个库
-            return "db" + ((id % 8) + 8);
-        }
-        if (id < 160) {
-            //32个库
-            return "db" + (id % 16);
-        }
-        //
-        throw new IllegalArgumentException("id out of range. id:" + id);
-    }
     //region 版本1-库分片计算规则
     //以用户id作为唯度
     private static String getDBInfoByUserId(int id) {
         if (id < 10) {
-            //2个库
+            //当只有一个数据库时是不需要进行库的分片计算的
             return "db" + (id % 2);
         }
         if (id < 20) {
-            //4个库
+            //2个库
             return "db" + ((id % 2) + 2);
         }
         if (id < 40) {
-            //8个库
+            //4个库
             return "db" + ((id % 4) + 4);
         }
         if (id < 80) {
-            //16个库
+            //8个库
             return "db" + ((id % 8) + 8);
         }
         if (id < 160) {
-            //32个库
+            //16个库
             return "db" + (id % 16);
         }
         //
-        throw new IllegalArgumentException("id out of range. id:" + id);
+        throw new IllegalArgumentException("[dbRule]id out of range. id:" + id);
     }
     //endregion
 }
