1d4179a5a4a489b5ab8d1318e0a5eae1e7556415
yzd <tedijoteamo@163.com>
2017-12-29 15:00:07 +0800

    1,java二维码生成-谷歌（Google.zxing）开源二维码生成的实例及介绍

---
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/QrCodeExt/_Zreadme.java
@@ -0,0 +1,11 @@
+
+
+
+//参考：
+//java二维码生成-谷歌（Google.zxing）开源二维码生成的实例及介绍
+//https://www.cnblogs.com/lanxiamo/p/6293580.html
+//mult-media(audio/image/qrcode) support web service (多媒体处理的web服务, 二维码, 音频, 图片服务支持)
+//https://github.com/yaozd/quick-media
+//https://github.com/liuyueyi/quick-media
+//Java 实现二维码及有Logo 的二维码（SpringMVC+Zxing+Jsp）
+//http://blog.csdn.net/wenteryan/article/details/51373712
