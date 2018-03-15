e43dcdda5b6af0be71461499dbcf728b2ad7b0ad
yzd <tedijoteamo@163.com>
2018-03-15 11:13:18 +0800

    //只对公共缓存资源使用，因为有公共的数据才会出现热点问题。     //对于公共缓存资源可以采用计数轮询的方式进行水平扩展

---
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/loadBalance/CacheRoundRobin.java
@@ -0,0 +1,39 @@
+package com.yzd.jutils.loadBalance;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CacheRoundRobin {
+    private static CacheRoundRobin ourInstance = new CacheRoundRobin();
+
+    public static CacheRoundRobin getInstance() {
+        return ourInstance;
+    }
+
+    private CacheRoundRobin() {
+    }
+    private static Map<String, AtomicLong> configMap = new HashMap<String, AtomicLong>();
+    //目前这种属于人为的水平扩展-简单有效
+    //只对公共缓存资源使用，因为有公共的数据才会出现热点问题。
+    //对于公共缓存资源可以采用计数轮询的方式进行水平扩展
+    //key的选择是“P01.Other1SelectAll 主名”这样就可以控制KEY的数量
+    //P01.Other1SelectAll 主名
+    //P01.Other1SelectAll:1di41n95bd34:0848e79ab3873df627ab8e1d3e13a61a 主名+WHERE的MD5的值
+    public Long getValue(String key) {
+        if(!configMap.containsKey(key)) {
+            initConfig(key);
+        }
+        Long number=configMap.get(key).incrementAndGet();
+        if(number>10L){
+            configMap.put(key,new AtomicLong(0));
+        }
+        return number;
+    }
+    private synchronized void initConfig(String key) {
+        configMap.put(key, new AtomicLong(0));
+    }
+}
--- com.yzd.jutils/src/main/java/com/yzd/jutils/loadBalance/Test.java
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/loadBalance/Test.java
@@ -32,4 +32,22 @@ public class Test {
         });
        System.out.print(RoundRobin.getServer());
     }
+    @org.junit.Test
+    public void CacheRoundRobin_T1(){
+
+        for (int i = 0; i <100 ; i++) {
+            Long value=CacheRoundRobin.getInstance().getValue("P01.Other1SelectAll");
+            //如果对"主名=P01.Other1SelectAll"的KEY进行水平扩展5倍的话就需要对5进行取余
+            //取余结果是 0，1，2，3，4；
+            //然后将编号放在KEY名称的后面
+            //P01.Other1SelectAll:1di41n95bd34:0848e79ab3873df627ab8e1d3e13a61a 转化为
+            //P01.Other1SelectAll:1di41n95bd34:0848e79ab3873df627ab8e1d3e13a61a.0;P01.Other1SelectAll:1di41n95bd34:0848e79ab3873df627ab8e1d3e13a61a.1
+            //P01.Other1SelectAll:1di41n95bd34:0848e79ab3873df627ab8e1d3e13a61a.2;P01.Other1SelectAll:1di41n95bd34:0848e79ab3873df627ab8e1d3e13a61a.3
+            //P01.Other1SelectAll:1di41n95bd34:0848e79ab3873df627ab8e1d3e13a61a.4
+            //这样就水平扩展5倍。
+            Long t1=value%5;
+            System.out.println(value);
+            System.out.println("t1="+t1);
+        }
+    }
 }
