2ea448f7f5768dfe505d04a24a2c6e2fc3b0b440
yzd <tedijoteamo@163.com>
2018-03-23 11:18:46 +0800

    增加两个示例

---
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/springBoot/excludefilterExt/SimpleTest.java
@@ -0,0 +1,30 @@
+package com.yzd.jutils.springBoot.excludefilterExt;
+
+/*
+import com.hb.insure.pay.backend.schedule.consumer.job.statement._StatementJob;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SimpleTestConfig.class)
+public class SimpleTest {
+    @MockBean
+    private _StatementJob myTestBean;
+    @Test
+    public void test() {
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertThat(true);
+    }
+}
+*/
