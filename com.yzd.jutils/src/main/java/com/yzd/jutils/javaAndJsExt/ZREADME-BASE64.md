2cd2cc94b76f56138e7b2b2a460bf2c81ea05b5f
yaozd <tedijoteamo@163.com>
2019-02-25 16:55:12 +0800

    js、java的base64相互加密、解密,用于前后台数据传输

---
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/javaAndJsExt/ZREADME-BASE64.md
@@ -0,0 +1,13 @@
+## js、java的base64相互加密、解密,用于前后台数据传输
+
+####  适用场景：
+```
+1、js、ajax前端传参到后端，中文乱码问题；
+2、js、ajax前端json传参到后端，json参数含有特殊字符报错问题；
+提醒：所以提前用base64处理json字符串或参数
+
+```
+
+> 参考：
+- [https://github.com/yckart/jquery.base64.js](https://github.com/yckart/jquery.base64.js)-支持中文-已验证byArvin
+- [jquery-base64.js插件使用](https://www.cnblogs.com/windseek/p/6433606.html)
---
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/javaAndJsExt/_MainTest.java
@@ -0,0 +1,54 @@
+package com.yzd.jutils.javaAndJsExt;
+
+import java.io.UnsupportedEncodingException;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class _MainTest {
+    // 加密
+    public static String encode(String str) {
+        byte[] b = null;
+        String s = null;
+        try {
+            b = str.getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        if (b != null) {
+            s = new BASE64Encoder().encode(b);
+            //据RFC 822规定，每76个字符，还需要加上一个回车换行去掉换行符
+            s = s.replaceAll("[\\s*\t\n\r]", "");
+        }
+        return s;
+    }
+
+    // 解密
+    public static String decode(String s) {
+        byte[] b = null;
+        String result = null;
+        if (s != null) {
+            BASE64Decoder decoder = new BASE64Decoder();
+            try {
+                b = decoder.decodeBuffer(s);
+                result = new String(b, "utf-8");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Base64-java版
+     * @param args
+     */
+    public static void main(String[] args){
+        //使用统一的UTF-8编码
+        String s = "你好world";
+        //System.out.println(s.length());
+        String enStr = encode(s);
+        String deStr = decode(enStr);
+        System.out.println("原始数据："+s+"\n加密数据："+enStr+"\n解密数据："+deStr);
+        System.out.println("5L2g5aW9d29ybGQ=".equals(enStr));
+    }
+}
