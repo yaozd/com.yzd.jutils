ec4e45ff63bb5784fab4cf84840ce8e76eb93663
yaozd <tedijoteamo@163.com>
2019-04-16 13:26:10 +0800

    Ketttle-分页循环导入-05-设置ETL-UserInfoTb-正式版-参数来自上级job

--- com.yzd.jutils/src/main/java/com/yzd/jutils/dataSync/kettle/ZREADME-kettle-示例.md
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/dataSync/kettle/ZREADME-kettle-示例.md
@@ -1,2 +1,7 @@
 ### ZREADME-kettle示例-byArvin
 - t1-Javascript实现循环示例-Run-Main
+- t2-Javascript与Sql语句结合实现循环示例-Run-Main
+### 下载地址：百度云-》开发工具-》S-数据同步-kettle
+- kettle示例-实现分页循环导入-2019-04-16-1055
+- Javascript与Sql语句结合实现分页循环导入-Run-Main
+- 分页循环导入-05-设置ETL-UserInfoTb-正式版-参数来自上级job.zip
--- com.yzd.jutils/src/main/java/com/yzd/jutils/dataSync/kettle/fun.js
+++ com.yzd.jutils/src/main/java/com/yzd/jutils/dataSync/kettle/fun.js
@@ -1,9 +1,10 @@
 //用于写javascript脚本
 
 var currPage=getCurrPage();
-
+var startRow=getStartRow(currPage);
 if(currPage<=6){
     parent_job.setVariable("CURR_PAGE",currPage+1);
+    parent_job.setVariable("START_ROW",startRow);
     true;
 }
 if(currPage>6){
@@ -17,4 +18,36 @@ function getCurrPage() {
         return 1;
     }
     return parseInt(currPage);
+}
+function getStartRow(currPage) {
+    var pageSize=100;
+    var startRow=currPage*pageSize;
+    return startRow;
+}
+//==============================================================
+//最多循环1000次，避免出现死循环
+var currPage=getCurrPage();
+var startRow=getStartRow(currPage);
+if(currPage<=1000){
+    parent_job.setVariable("CURR_PAGE",currPage+1);
+    parent_job.setVariable("START_ROW",startRow);
+    true;
+}
+if(currPage>1000){
+    false;
+}
+
+//
+function getCurrPage() {
+    var currPage = parent_job.getVariable("CURR_PAGE");
+    if (currPage == null || currPage == undefined || currPage == "") {
+        return 1;
+    }
+    return parseInt(currPage);
+}
+function getStartRow(currPage) {
+    var pageSize=parent_job.getVariable("PAGE_SIZE");
+    //var pageSize=100;
+    var startRow=currPage*pageSize;
+    return startRow;
 }
